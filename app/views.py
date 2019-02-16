8e3dc103508bf084668051785b2821a66ad57817
Alicia Melgar <alimelgon27@gmail.com>
2019-02-16 18:23:54 +0100

    CryptobookSesion5

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 def index():
-    return 'Portada de mi app web'
+    return render_template('index.html')
+@app.route ('/nuevacompra', methods=['GET','POST'])
+def nuevacompra():
+    return render_template('nuevacompra.html') #No puedo poner index.html porque me vuelve a la misma ventana, otro vacio
+
--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ app = Flask(__name__) #le paso el nombre de nuestro fichero, aplcación
 @app.route('/')
 def hello_world():
     return 'Hola, mundo'
+
